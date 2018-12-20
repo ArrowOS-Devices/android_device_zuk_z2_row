08907b4096101d6a1bc249a891845ccd0167d310
kubersharma001 <kubersharma001@gmail.com>
2018-12-20 17:59:21 +0530

    z2_row: adapt for arrow

--- arrow_z2_row.mk
+++ arrow_z2_row.mk
@@ -21,10 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from z2_row device
 $(call inherit-product, device/zuk/z2_row/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
-PRODUCT_NAME := lineage_z2_row
+PRODUCT_NAME := arrow_z2_row
 PRODUCT_DEVICE := z2_row
 PRODUCT_MANUFACTURER := ZUK
 PRODUCT_BRAND := ZUK
